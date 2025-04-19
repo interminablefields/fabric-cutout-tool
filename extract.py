c189f696cea267f72505f18a8595a584acd0c01c
janya <budaraju.janya@gmail.com>
2025-04-19 18:47:09 -0400

    docs: pseudocode / problem setup

---
+++ extract.py
@@ -0,0 +1,8 @@
+'''
+1. color segmentation: separate the fabric from background
+2. color segmentation: separate the cutout from the fabric
+3. extract the fabric. remove all polygons/circles from within it, probably.
+4. scale using nickel for reference.
+5. use some sort of packing algorithm on the leftover fabric?
+
+'''
